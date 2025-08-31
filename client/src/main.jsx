6e8e7d6df0c24f8f220c74d4cbc16ba5f0bdc13d
Boss <rangerover@email.com>
2025-08-31 10:25:54 +0530

    Added VercelJson

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import {ClerkProvider} from '@clerk/clerk-react'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -14,8 +14,8 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <App />
-  </ClerkProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </BrowserRouter>,
 )
